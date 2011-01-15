56636cd5ce825beb88ae443600944c33b949be27
Donny Kurnia <donnykurnia@gmail.com>
2011-01-15 11:59:45 +0700

    fix README file format
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- railscasts-downloader.rb
+++ railscasts-downloader.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+###############################################################################
+#
+# RailsCasts Downloader 0.1
+#
 # Copyright (C) 2011 Donny Kurnia <donnykurnia@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify it under
@@ -15,26 +19,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, see <http://www.gnu.org/licenses>.
 #
-# Linking [name of your program] statically or dynamically with other modules
-# is making a combined work based on [name of your program]. Thus, the terms
-# and conditions of the GNU General Public License cover the whole combination.
-#
-# In addition, as a special exception, the copyright holders of [name of your
-# program] give you permission to combine [name of your program] with free
-# software programs or libraries that are released under the GNU LGPL and with
-# code included in the standard release of [name of library] under the [name of
-# library's license] (or modified versions of such code, with unchanged
-# license). You may copy and distribute such a system following the terms of
-# the GNU GPL for [name of your program] and the licenses of the other code
-# concerned{, provided that you include the source code of that other code when
-# and as the GNU GPL requires distribution of source code}.
-#
-# Note that people who make modified versions of [name of your program] are not
-# obligated to grant this special exception for their modified versions; it is
-# their choice whether to do so. The GNU General Public License gives
-# permission to release a modified version without this exception; this
-# exception also makes it possible to release a modified version which carries
-# forward this exception.
+###############################################################################
 
 #load required module
 require 'open-uri'
