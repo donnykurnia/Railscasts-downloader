243e647428f5e08f658ecd862dcaf72b302fafe9
Donny Kurnia <donnykurnia@gmail.com>
2011-01-15 11:39:28 +0700

    add GPL license
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- railscasts-downloader.rb
+++ railscasts-downloader.rb
@@ -1,5 +1,41 @@
 #!/usr/bin/env ruby
 
+# Copyright (C) 2011 Donny Kurnia <donnykurnia@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses>.
+#
+# Linking [name of your program] statically or dynamically with other modules
+# is making a combined work based on [name of your program]. Thus, the terms
+# and conditions of the GNU General Public License cover the whole combination.
+#
+# In addition, as a special exception, the copyright holders of [name of your
+# program] give you permission to combine [name of your program] with free
+# software programs or libraries that are released under the GNU LGPL and with
+# code included in the standard release of [name of library] under the [name of
+# library's license] (or modified versions of such code, with unchanged
+# license). You may copy and distribute such a system following the terms of
+# the GNU GPL for [name of your program] and the licenses of the other code
+# concerned{, provided that you include the source code of that other code when
+# and as the GNU GPL requires distribution of source code}.
+#
+# Note that people who make modified versions of [name of your program] are not
+# obligated to grant this special exception for their modified versions; it is
+# their choice whether to do so. The GNU General Public License gives
+# permission to release a modified version without this exception; this
+# exception also makes it possible to release a modified version which carries
+# forward this exception.
+
 #load required module
 require 'open-uri'
 require 'nokogiri'
