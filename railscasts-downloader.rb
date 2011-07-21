425a792459a9f52c31db20e4eba84680df50ffc0
Donny Kurnia <donny.k@kiranatama.com>
2011-07-21 16:24:54 +0700

    handle new format of railscasts website
    Signed-off-by: Donny Kurnia <donny.k@kiranatama.com>

--- railscasts-downloader.rb
+++ railscasts-downloader.rb
@@ -2,7 +2,7 @@
 
 ###############################################################################
 #
-# RailsCasts Downloader 0.1
+# RailsCasts Downloader 0.2
 #
 # Copyright (C) 2011 Donny Kurnia <donnykurnia@gmail.com>
 #
@@ -21,38 +21,68 @@
 #
 ###############################################################################
 
-#load required module
+#load required modules
 require 'open-uri'
 require 'nokogiri'
+require 'yaml'
 
-#get existing episodes
-existing_filenames = Dir.glob('*.mov')
-existing_eps_numbers = existing_filenames.map {|x| "/episodes/" + x[/\d+/].sub!(/^0*/, "") + "-"}
+#constants
+BASE_URL = 'http://railscasts.com'
+VIDEO_PAGE_CACHE_FILE = 'video_page_cache.yml'
 
-#get videos pages url from archive page
-video_pages = []
-railscasts_archive_page = "http://railscasts.com/episodes/archive"
-doc = Nokogiri::HTML(open(railscasts_archive_page))
-doc.search('//*[@href]').each do |m|
-  episode = "http://railscasts.com" + m[:href] if m[:href].include? "episodes" 
-  video_pages << episode unless episode.nil? or existing_eps_numbers.any? { |eps| episode.include? eps }
-end
-
-#load each video pages, then download the video
-video_pages.each do |page|
+#functions
+def get_video_pages(page, video_pages, existing_eps_numbers, video_page_cache)
+  cache_hit = false
+  p "Reading #{page} ..."
   doc = Nokogiri::HTML(open(page))
   doc.search('//*[@href]').each do |m|
-    video_url = m[:href] if m[:href].match ".mov$"
-    unless video_url.nil?
-      filename = video_url.split('/').last
+    episode = BASE_URL + m[:href] if m[:href].include?("episodes") && ! m[:href].include?("?")
+    unless episode.nil? or existing_eps_numbers.any? { |eps| episode.include? eps } or video_pages.include?(episode)
+      video_pages << episode
+      cache_hit = video_page_cache.include? episode
+    end
+  end
+  next_page_link = doc.search('//a[@class = "next_page"]')[0]
+  if next_page_link and ! cache_hit
+    video_pages = get_video_pages(BASE_URL + next_page_link[:href], video_pages, existing_eps_numbers, video_page_cache)
+  end
+  video_pages
+end
 
-      p "Downloading #{filename}"
-      %x(wget #{video_url} -c -O #{filename}.tmp )
-      %x(mv #{filename}.tmp #{filename} )
-      p "Finish downloading #{filename}"
+def download_videos(video_pages)
+  video_pages.each do |page|
+    doc = Nokogiri::HTML(open(page))
+    doc.search('//*[@href]').each do |m|
+      video_url = m[:href] if m[:href].match ".mp4$"
+      unless video_url.nil?
+        filename = video_url.split('/').last
+
+        p "Downloading #{filename}"
+        %x(wget #{video_url} -c -O #{filename}.tmp )
+        %x(mv #{filename}.tmp #{filename} )
+        p "Finish downloading #{filename}"
+      end
     end
   end
 end
 
-p 'Finished synchronization'
+#load video page cache
+video_page_cache = YAML::load(File.open(VIDEO_PAGE_CACHE_FILE, File::RDONLY|File::CREAT)) || []
 
+#get existing episodes
+existing_filenames = Dir.glob('*.mp4')
+existing_eps_numbers = existing_filenames.map {|x| "/episodes/" + x[/\d+/].sub!(/^0*/, "") + "-"}
+
+#get videos pages url from Railscasts pages
+video_pages = get_video_pages(BASE_URL, [], existing_eps_numbers, video_page_cache)
+
+#write back the cache
+video_page_cache = video_pages | video_page_cache
+File.open(VIDEO_PAGE_CACHE_FILE, 'w') do |f|
+  f.write(video_page_cache.to_yaml)
+end
+
+#load each video pages, then download the video
+#download_videos(video_pages)
+
+p 'Finished synchronization'
